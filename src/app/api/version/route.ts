5208e80bafcb09266f8f2df1938e2b9d1188b35c
cvs0 <143862758+cvs0@users.noreply.github.com>
2024-05-05 18:18:36 -0600

    Bug fix
    Marked the GET function for the API as async instead of default.

--- src/app/api/version/route.ts
+++ src/app/api/version/route.ts
@@ -2,7 +2,7 @@ import { getLatestVersion } from "@/lib/constants";
 import { currentUser } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
-export default function GET(
+export async function GET(
     req: Request
 ) {
     try {
