5170cf1d900ca2f41066f41236fed6bd9c3c2fe2
cvs0 <143862758+cvs0@users.noreply.github.com>
2024-05-12 18:18:21 -0600

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware
 // for more information about configuring your Middleware
 export default authMiddleware({
-  // Allow signed out users to access the specified routes:
-  // publicRoutes: ['/anyone-can-visit-this-route'],
+  publicRoutes: ['/', '/docs'],
 });
  
 export const config = {
